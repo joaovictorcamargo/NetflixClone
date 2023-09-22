d565e640bcd983eead9f27bcc2d3f50b14e18e11
João Victor Camargo Silva <joao.victor@globalsys.com>
2023-09-22 17:24:22 -0300

    feat: input component

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,3 +1,5 @@
+import Input from "@/components/Input";
+
 const Auth = () => {
   const divStyle = {
     backgroundImage: `url('/images/hero.jpg')`,
@@ -10,14 +12,21 @@ const Auth = () => {
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <img src="/images/logo.png" style={{ height: "44px" }} alt="Logo" />
-          </nav>
-          <div className="flex justify-center">
-            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sign In
-            </h2>
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+            <div className="flex flex-col gap-4">
+              <Input
+                id="name"
+                type="text"
+                label="Username"
+                value={"oi"}
+                onChange={undefined}
+              />
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
