a3c9dd46eaef34570301fa0c9836b4d7fb202223
João Victor Camargo Silva <joao.victor@globalsys.com>
2023-09-22 17:14:33 -0300

    refact: app auth page

---
+++ app/auth/page.tsx
@@ -0,0 +1,26 @@
+const Auth = () => {
+  const divStyle = {
+    backgroundImage: `url('/images/hero.jpg')`,
+    height: "100vh",
+    width: "100%",
+  };
+
+  return (
+    <div style={divStyle}>
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" style={{ height: "44px" }} alt="Logo" />
+          </nav>
+          <div className="flex justify-center">
+            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              Sign In
+            </h2>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
