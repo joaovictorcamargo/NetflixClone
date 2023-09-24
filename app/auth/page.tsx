fa8a8d8a3125a13ace58d1383f43ff815e3a63bd
João Victor Camargo Silva <joao.victor@globalsys.com>
2023-09-24 08:29:00 -0300

    feat: prisma init

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,18 +1,52 @@
 "use client"; // This is a client component 👈🏽
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Input from "@/components/Input";
 
 const Auth = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
 
   const divStyle = {
     backgroundImage: `url('/images/hero.jpg')`,
     height: "100vh",
     width: "100%",
   };
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      // await signIn('credentials', {
+      //   email,
+      //   password,
+      //   redirect: false,
+      //   callbackUrl: '/'
+      // });
+      // router.push('/profiles');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password]);
+
+  const register = useCallback(async () => {
+    try {
+      // await axios.post('/api/register', {
+      //   email,
+      //   name,
+      //   password
+      // });
+      // login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div style={divStyle}>
@@ -22,33 +56,53 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {" "}
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input
-                id="name"
-                type="text"
-                label="Username"
-                value={"oi"}
-                onChange={(e: any) => setName(e.target.value)} 
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={"oi"}
+                  onChange={(e: any) => setName(e.target.value)}
                 />
-               <Input
+              )}
+              <Input
                 id="email"
                 type="email"
                 label="Email address or phone number"
                 value={"oi"}
-                onChange={(e: any) => setEmail(e.target.value)} 
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                type="password" 
-                id="password" 
-                label="Password" 
+                type="password"
+                id="password"
+                label="Password"
                 value={"oi"}
-                onChange={(e: any) => setPassword(e.target.value)} 
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button onClick={() => {}} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              Login
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+              .
+            </p>
           </div>
         </div>
       </div>
